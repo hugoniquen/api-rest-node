846952900f21f4c4e7feaf668562ddb394421d3e
Hugo.Niquen <hugo.niquen@bm-maritima.pe>
2024-01-30 15:11:23 -0500

    Se agrego el metodo getTaskById

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -36,6 +36,17 @@ const taskController = {
     },
 
     // Obtener una tarea por Id
+    getTaskById: async(req, res)=>{
+        try {
+            const task = await Task.findById(req.params.id);
+            if (!task) {
+                res.status(404).json({message: "No existe el recurso"});
+            }
+            res.status(200).json(task);    
+        } catch (error) {
+            res.status(500).json({message : error.message});
+        }
+    },
 
     // Actualizar una tarea
 
--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -4,5 +4,6 @@ const taskController = require('../controllers/taskController');
 
 router.post('/', taskController.createTask);
 router.get('/', taskController.getAllTask);
+router.get('/:id', taskController.getTaskById);
 
 module.exports = router;
