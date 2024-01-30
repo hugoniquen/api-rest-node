c47ca1e7e23bd7a7dc74e34e47737b79349fe6d1
Hugo.Niquen <hugo.niquen@bm-maritima.pe>
2024-01-30 13:15:36 -0500

    add servicios

---
+++ README.md
@@ -0,0 +1,8 @@
+npm install express -S
+npm install mongoose -S
+npm install nodemon -D
+
+Miguel Ramos
+https://github.com/MiguelAngelRamos/api-rest-node
+GitHub - MiguelAngelRamos/api-rest-node: Api rest con node y express
+Api rest con node y express. Contribute to MiguelAngelRamos/api-rest-node development by creating an account on GitHub.
--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');   //ORM
+const express = require("express");
+const mongoose = require("mongoose"); //ORM
+const cors = require('cors');
 
+require("dotenv").config();
+
+const app = express();
+
+// Middlware de Cors y parse JSON
+app.use(cors());
+app.use(express.json());
+
+// Conexion a BD MongoDB
+console.log(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+  })
+  .catch((err) => console.error("No se logro conectar a la BD Mongo"));
+
+  const PORT = process.env.PORT || 8080;
+
+  app.listen(PORT, ()=>{
+    console.log(`Servidor ejecutandose en el puerto ${PORT}`);
+  });
