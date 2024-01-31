bc6bf013c0f95404d15c8d419218ef4cd33467f6
hugoniquen <hugo.niquen@bm-maritima.pe>
2024-01-31 13:18:29 -0500

    Controlador de usuario

---
+++ controllers/authController.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const generaToken = (user) =>{
+
+    return jwt.sign(
+        {
+            id: user._id,
+            username: user.username,
+            roles: user.roles,
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: '1hr'}
+    );
+};
+
+const authController = {
+    register: async(req, res)=>{
+        try {
+            
+            const newUser = new  User(req.body);
+            await newUser.save();
+            const token = generaToken(newUser);
+
+            restart.status(201).json({token, user: newUser});
+
+        } catch (error) {
+            restart.status(500).json({message: error});
+        }
+    },
+
+    login: async(req, res)=>{
+        try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email});
+            if (!user) {
+                return res.status(401).json({message: 'Usuario no encontrado'});
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if (!passwordMatch) {
+                return res.status(401).json({message: 'Password incorrecta'});
+            }
+            const token = generaToken(user);
+            res.status(200).json({token, user});
+
+        } catch (error) {
+            res.status(400).json({message: error});
+        }
+    }
+};
+
+module.exports = authController;
