ae7f04fd2b069b25c8c6e3e667cbdcaf4a5dd166
hugoniquen <hugo.niquen@bm-maritima.pe>
2024-01-31 12:42:38 -0500

    .

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Shema({
+    username:{
+        type: String,
+        required : true,
+    },
+    email:{
+        type: String,
+        required : true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required : true,
+    },
+    roles:{
+        type: [String],
+        default: ['USER'], // USER, ADMIN
+    },
+});
+
+// Metodo para cifrar la contraseña antes de guardarla
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password  =await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports   = mongoose.model('User', userSchema);
